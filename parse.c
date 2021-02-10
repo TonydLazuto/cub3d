cfe2f8d83e275cdb6ba09fb388d7473aeab5fdf6
Tonyd <aderose73@gmail.com>
2021-02-11 00:04:07 +0100

    refactoring fill_map()

--- parse.c
+++ parse.c
@@ -1,24 +1,6 @@
 #include "cub3d.h"
 
-char	**fill_map(char **file_lines, char **map, size_t len_map, size_t start)
-{
-	size_t	i;
-
-	i = 0;
-	if (!(map = malloc(sizeof(char*) * (len_map + 1))))
-		return (NULL);
-	while (file_lines[start])
-	{
-		if (!(map[i] = ft_strdup(file_lines[start])))
-			return (NULL);
-		start++;
-		i++;
-	}
-	map[i] = NULL;
-	return (map);
-}
-
-int		fill_params_and_map(char **file_lines, t_cub *cub, size_t len_params)
+static int		fill_map(char **file_lines, t_cub *cub, size_t len_params)
 {
 	size_t	len_map;
 	size_t	i;
@@ -28,17 +10,20 @@ int		fill_params_and_map(char **file_lines, t_cub *cub, size_t len_params)
 	while (file_lines[len_map])
 		len_map++;
 	len_map -= len_params;
-	if (!(cub->map = fill_map(file_lines, cub->map, len_map, len_params)))
+	if (!(cub->map = malloc(sizeof(char*) * (len_map + 1))))
 		return (-1);
-	while (i < len_map)
+	while (file_lines[len_params])
 	{
-		printf("%s\n", cub->map[i]);
+		if (!(cub->map[i] = ft_strdup(file_lines[len_params])))
+			return (-1);
+		len_params++;
 		i++;
 	}
+	cub->map[i] = NULL;
 	return (0);
 }
 
-int		split_params_map(char *file, t_cub *cub)
+int				split_params_map(char *file, t_cub *cub)
 {
 	size_t	j;
 	size_t	len_params;
@@ -56,7 +41,8 @@ int		split_params_map(char *file, t_cub *cub)
 			break;
 		len_params++;
 	}
-	if (fill_params_and_map(file_lines, cub, len_params) == -1)
+	if (fill_map(file_lines, cub, len_params) == -1)
 		return (-1);
+	
 	return (0);
 }
