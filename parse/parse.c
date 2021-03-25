c9d8d57cd8ea6004e840ac3f7a32e88f97ea3d57
Tonyd <aderose73@gmail.com>
2021-03-25 17:58:03 +0100

    fix check_nb_params() fixed

--- parse/parse.c
+++ parse/parse.c
@@ -1,5 +1,21 @@
 #include "../cub3d.h"
+/*
+--> delete the comparison inside parse_resolution() in parse_params.c
+--> VOIR si mlx_get_screen_size() type "void" returns?
+static void		check_max_screen_size(t_cub *cub)
+{
+	int screen_width;
+	int screen_height;
 
+	screen_width = 0;
+	screen_height = 0;
+	mlx_get_screen_size(cub->mlx_ptr, &screen_width, &screen_height);
+	if (cub->width > screen_width)
+		cub->width = screen_width;
+	if (cub->height > screen_height)
+		cub->height = screen_height;
+}
+*/
 static int	fill_params(char **file_lines, t_cub *cub, size_t len_params)
 {
 	size_t	i;
@@ -45,39 +61,26 @@ static int	fill_map(char **file_lines, t_cub *cub, size_t len_params)
 	return (0);
 }
 
-static int	check_two_char(char first_char, char second_char, int nb_params)
+static int		check_nb_params(char** line, size_t len_params)
 {
-	if ((first_char == 'R' && second_char == ' ')
-		|| (first_char == 'S' && second_char == ' ')
-		|| (first_char == 'F' && second_char == ' ')
-		|| (first_char == 'C' && second_char == ' ')
-		|| (first_char == 'N' && second_char == 'O')
-		|| (first_char == 'S' && second_char == 'O')
-		|| (first_char == 'W' && second_char == 'E')
-		|| (first_char == 'E' && second_char == 'A'))
-		nb_params--;
-	return (nb_params);
-}
-
-static int	check_nb_params(char **file_lines, size_t len_params)
-{
-	int		nb_params;
-	size_t	i;
-	size_t	j;
+	int nb_params;
+	size_t i;
+	size_t j;
 
 	nb_params = 8;
 	i = 0;
-	printf("|%zu|\n", len_params);
-	if (len_params != 8)
-	{
-		ft_putendl_fd("Error\nNumber map paramaters.", 1);
-		return (-1);
-	}
 	while (i < len_params)
 	{
-		j = skip_space(file_lines[i], 0);
-		printf("|%s|\n", file_lines[i]);
-		nb_params = check_two_char(file_lines[i][j], file_lines[i][j + 1], nb_params);
+		j = skip_space(line[i], 0);
+		if ((line[i][j] == 'R' && line[i][j + 1] == ' ')
+            || (line[i][j] == 'S' && line[i][j + 1] == ' ')
+			|| (line[i][j] == 'F' && line[i][j + 1] == ' ')
+            || (line[i][j] == 'C' && line[i][j + 1] == ' ')
+			|| (line[i][j] == 'N' && line[i][j + 1] == 'O')
+            || (line[i][j] == 'S' && line[i][j + 1] == 'O')
+			|| (line[i][j] == 'W' && line[i][j + 1] == 'E')
+            || (line[i][j] == 'E' && line[i][j + 1] == 'A'))
+			nb_params--;
 		i++;
 	}
 	if (nb_params != 0)
